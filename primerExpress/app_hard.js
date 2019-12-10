242446b0131959c15d496736c124dd3428f9145c
Lopi <nicolasjlf@hotmail.com>
2019-12-10 15:31:44 -0300

    proyecto levantado con express generator

--- primerExpress/app_hard.js
+++ primerExpress/app_hard.js
@@ -7,16 +7,7 @@ const faqs = require('./src/preguntasFrequentes')
 
 fs = require('fs');
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
-});
+
 
 http.createServer((req, res) => {
 	res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
--- primerExpress/routes/movie.js
+++ primerExpress/routes/movie.js
@@ -8,12 +8,20 @@ const movies = require('../src/movies')
 router.get('/all/masvotadas', (request,response)=> {
     response.send(movies.masVotadas)
 })
+
+//hago un ruteo dinamico con una query
+router.get('/buscar', (request,response)=> {
+    console.log(request.query)
+    response.send(movies.buscarPelicula(request.query.q))
+})
+
 //hago un ruteo dinamico con la posicion de la pelicula
 router.get('/:position', (request,response)=> {
     response.send(movies.traerPeliculaEnPosicion(request.params.position))
 })
-
+//hago una query basado en un path
 router.get('/search/:query', (request,response)=> {
+    console.log(request.params.query)
     response.send(movies.buscarPelicula(request.params.query))
 })
 //exporto el modulo
